f2a0fbfe09125bda596cc7ab4f6758d6e897a26e
Muhammad <mohamedhossameldin78@gmail.com>
2025-04-03 16:22:19 +0200

    Create Mutual exclusion feature (Mutex) module.

---
+++ MoRTOS/Libraries/RTOS/inc/rtos_mutex.h
@@ -0,0 +1,74 @@
+/**
+ * @file rtos_mutex.h
+ * @brief Header file for RTOS mutex management.
+ *
+ * This file contains the definitions, macros, and function prototypes
+ * required for managing mutexes in the RTOS.
+ *
+ * @author Mohamed HossamElDin
+ * @date 3 April 2025
+ */
+
+/**
+ * @defgroup RTOS_Mutex RTOS Mutex Management
+ * @brief Group for RTOS mutex-related definitions and functions.
+ * @{
+ */
+
+#ifndef RTOS_MUTEX_H
+#define RTOS_MUTEX_H
+
+// Includes
+#include <stdint.h>
+
+/**
+ * @defgroup RTOS_Mutex_exported_Defines Defines
+ * @brief Exported Defines used in RTOS mutex management.
+ * @{
+ */
+
+/** @} */ // End of RTOS_Mutex_exported_Defines group
+
+/**
+ * @defgroup RTOS_Mutex_exported_Macros Macros
+ * @brief Macros used in RTOS mutex management.
+ * @{
+ */
+
+/** @} */ // End of RTOS_Mutex_Macros group
+
+/**
+ * @defgroup RTOS_Mutex_exported_Constants Constants
+ * @brief Constants used in RTOS mutex management.
+ * @{
+ */
+
+/** @} */ // End of RTOS_Mutex_exported_Constants group
+
+/**
+ * @defgroup RTOS_Mutex_exported_Typedefs Typedefs
+ * @brief Typedefs used in RTOS mutex management.
+ * @{
+ */
+
+/** @} */ // End of RTOS_Mutex_exported_Typedefs group
+
+/**
+ * @defgroup RTOS_Mutex_exported_Variables extern Variables
+ * @brief Extern Variables used in RTOS mutex management.
+ * @{
+ */
+
+/** @} */ // End of RTOS_Mutex_exported_Variables group
+
+/**
+ * @defgroup RTOS_Mutex_exported_Functions Function Prototypes
+ * @brief Function prototypes for RTOS mutex management.
+ * @{
+ */
+
+/** @} */ // End of RTOS_Mutex_exported_Functions group
+
+#endif // RTOS_MUTEX_H
+
+/** @} */ // End of RTOS_Mutex group
---
+++ MoRTOS/Libraries/RTOS/src/rtos_mutex.c
@@ -0,0 +1,83 @@
+/**
+ * @file rtos_mutex.c
+ * @brief Implementation of RTOS mutex utilities.
+ * 
+ * @author Mohamed HossamElDin
+ * @date 20 March 2025
+ */
+
+// -----------------------------------------------------------------------------
+// Includes
+
+// -----------------------------------------------------------------------------
+// Private Typedefs
+// -----------------------------------------------------------------------------
+/**
+ * @defgroup RTOS_Mutex_Private_Typedefs Private Typedefs
+ * @brief Typedefs used internally in RTOS mutex implementation.
+ * @{
+ */
+/** @} */
+
+// -----------------------------------------------------------------------------
+// Private Defines
+// -----------------------------------------------------------------------------
+/**
+ * @defgroup RTOS_Mutex_Private_Defines Private Defines
+ * @brief Macro definitions used internally in RTOS mutex implementation.
+ * @{
+ */
+/** @} */
+
+// -----------------------------------------------------------------------------
+// Private Macros
+// -----------------------------------------------------------------------------
+/**
+ * @defgroup RTOS_Mutex_Private_Macros Private Macros
+ * @brief Macros used internally in RTOS mutex implementation.
+ * @{
+ */
+/** @} */
+
+// -----------------------------------------------------------------------------
+// Private Constants
+// -----------------------------------------------------------------------------
+/**
+ * @defgroup RTOS_Mutex_Private_Constants Private Constants
+ * @brief Constants used internally in RTOS mutex implementation.
+ * @{
+ */
+/** @} */
+
+// -----------------------------------------------------------------------------
+// Private Variables
+// -----------------------------------------------------------------------------
+/**
+ * @defgroup RTOS_Mutex_Private_Variables Private Variables
+ * @brief Variables used internally in RTOS mutex implementation.
+ * @{
+ */
+/** @} */
+
+// -----------------------------------------------------------------------------
+// Private Functions
+// -----------------------------------------------------------------------------
+/**
+ * @defgroup RTOS_Mutex_Private_Functions Private Functions
+ * @brief Static Functions used internally in RTOS mutex implementation.
+ * @{
+ */
+/** @} */
+
+
+// -----------------------------------------------------------------------------
+// Function Implementations
+// -----------------------------------------------------------------------------
+/**
+ * @defgroup RTOS_Mutex_Implementations RTOS Mutex Function Implementations
+ * @brief Implementations of RTOS mutex functions.
+ * @{
+ */
+
+
+/** @} */
